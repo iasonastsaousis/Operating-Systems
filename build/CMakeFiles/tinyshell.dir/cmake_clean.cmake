aa841bb87a71edf3cbd8029ed801994a3dce8749
Jason <iasonas@tsaousis.net>
2025-11-16 02:50:53 +0200

    additions

---
+++ build/CMakeFiles/tinyshell.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tinyshell.dir/src/main.c.o"
+  "CMakeFiles/tinyshell.dir/src/main.c.o.d"
+  "tinyshell"
+  "tinyshell.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/tinyshell.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
